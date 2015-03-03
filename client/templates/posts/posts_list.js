59b8f0d03ec2c781048dda13bce216d2044690e8
jonas28 <jonas@steps.fm>
2015-03-03 23:56:19 +0100

    Setup a domain helper on the postItem.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,3 +1,17 @@
-/**
- * Created by jonaswegener on 03.03.15.
- */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
